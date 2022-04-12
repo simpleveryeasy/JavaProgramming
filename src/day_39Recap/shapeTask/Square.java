119d6b5ab8b6fdc7ef72721e74d37ecf4ece98ec
suleyman <simpleveryeasy@gmail.com>
2022-04-13 02:56:11 +0300

    Commit_130422

--- src/day_39Recap/shapeTask/Square.java
+++ src/day_39Recap/shapeTask/Square.java
@@ -1,11 +1,51 @@
 package day_39Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
 
+    //instance variable that can not be inherited from parent class shape
+    private double side;
 
+    //getter for variable side
+    public double getSide() {
+        return side;
+    }
 
+    //setter for variable side
+    public void setSide(double side) {
+        if (side <= 0){
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
 
+    // Construction call from parent class
+    public Square(double side) {
+        super("Squire");
+        setSide(side);
+    }
 
+    //area method overriding
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    //perimeter method overriding
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    //toString method overriding
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area='" + area() + '\'' +
+                ", perimeter'" + perimeter() + '\'' +
+                '}';
+    }
 }
 /*
 Square extends Shape:
