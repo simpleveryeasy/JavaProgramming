d8ce0a8e9ae18696d13c63e40ba239953e1b8e00
suleyman <simpleveryeasy@gmail.com>
2022-04-13 02:23:45 +0300

    Commit_130422

--- src/day_39Recap/shapeTask/Shape.java
+++ src/day_39Recap/shapeTask/Shape.java
@@ -36,6 +36,15 @@ public class Shape {
     public double perimeter(){
         return 0;
     }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter'" + perimeter() + '\'' +
+                '}';
+    }
 }
 /*
 Shape:
---
+++ src/day_39Recap/shapeTask/Square.java
@@ -0,0 +1,23 @@
+package day_39Recap.shapeTask;
+
+public class Square {
+
+
+
+
+
+}
+/*
+Square extends Shape:
+	variables:
+		side;
+
+	Encapsulate the field
+
+	Add a constructor to set the filed
+
+	area(): side * side
+	perimeter(): side * 4
+	toString(): side, area, perimeter
+
+ */
