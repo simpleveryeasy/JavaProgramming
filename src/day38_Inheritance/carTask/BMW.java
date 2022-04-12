e017cb20ee1018234e81295894c5846156d7f8a9
suleyman <simpleveryeasy@gmail.com>
2022-04-12 03:53:11 +0300

    Commit_120422

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,22 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand + " " + model + " breaks down");
+    }
+
+    public void racing(){
+        System.out.println(brand + " " + model + " is good for racing");
+    }
+}
+/*
+2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+ */
