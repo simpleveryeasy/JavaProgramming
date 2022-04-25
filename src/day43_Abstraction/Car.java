a9dadbc311d2fdd290b96f1a67d2c9214c83c8fb
simpleveryeasy <simpleveryeasy@gmail.com>
2022-04-25 18:52:30 +0300

    day43Abstraction_250422

---
+++ src/day43_Abstraction/Car.java
@@ -0,0 +1,65 @@
+package day43_Abstraction;
+
+public abstract class Car {
+
+    private final String brand, model;
+    private String color;
+    private final int year;
+    private double price;
+
+
+    public Car(String brand, String model, String color, int year, double price) {
+        this.brand = brand;
+        this.model = model;
+        setColor(color);
+        if (year < 1886){
+            throw new RuntimeException("Invalid Year: " + year);
+        }
+        this.year = year;
+        setPrice(price);
+    }
+
+    // getters
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+//setters (finals can't get setter)
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+
+        if (price <= 0){
+            throw new RuntimeException("Invalid Price: " + price);
+        }
+        this.price = price;
+    }
+
+
+
+//custom methods
+    public void stop(){
+        System.out.println("Press the brake");
+    }
+
+    public abstract void start();
+}
+
